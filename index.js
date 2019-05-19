9516d02996602e68fe634f360b60d53bbaccf282
RudraRajbanshi <rudra.raj.716@gmail.com>
2019-05-19 08:57:13 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const Express = require('express');
+const express = new Express();
+
+
+function login(request, response) {
+    response.json({ status: "ok" })
+}
+
+express.get('/api/login', login)
+
+//listen for connection
+express.listen(8000, 'localhost', function() {
+    console.log("server is running on port 8000")
+})
